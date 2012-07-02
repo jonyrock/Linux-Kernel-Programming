89949de3735590efb7b8487fe157ebcd04a04c92
Alexey Velikiy <gmpota@gmail.com>
2012-07-03 01:28:24 +0400

    Add net task as subdir

--- PR4.c
+++ PR4.c
@@ -1,79 +1,77 @@
-/*
- * Sample kobject implementation
- *
- * Copyright (C) 2004-2007 Greg Kroah-Hartman <greg@kroah.com>
- * Copyright (C) 2007 Novell Inc.
- *
- * Released under the GPL version 2 only.
- *
- */
 #include <linux/kobject.h>
 #include <linux/string.h>
 #include <linux/sysfs.h>
 #include <linux/module.h>
 #include <linux/init.h>
+#include <linux/sched.h>
 
 static int foo;
+char name_buffer[40];
 
 static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr,
 			char *buf)
 {
-	return sprintf(buf, "%d\n", foo);
+    return sprintf(buf, "%d\n", foo);
 }
 
+// Really do nothing. User can't update process name 
 static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,
-			 const char *buf, size_t count)
-{
-	sscanf(buf, "%du", &foo);
-	return count;
-}
+			 const char *buf, size_t count){ return 0; }
 
-static struct kobj_attribute foo_attribute =
-	__ATTR(foo, 0666, foo_show, foo_store);
+static struct kobj_attribute foo_attribute = __ATTR(foo, 0666, foo_show, foo_store);
 
-/*
- * Create a group of attributes so that we can create and destroy them all
- * at once.
- */
 static struct attribute *attrs[] = {
-	&foo_attribute.attr,
-	NULL,	/* need to NULL terminate the list of attributes */
+    &foo_attribute.attr,
+    NULL,
 };
 
-/*
- * An unnamed attribute group will put all of the attributes directly in
- * the kobject directory.  If we specify a name, a subdirectory will be
- * created for the attributes with the directory being the name of the
- * attribute group.
- */
 static struct attribute_group attr_group = {
-	.attrs = attrs,
+    .attrs = attrs,
 };
 
 static struct kobject *root_kobj;
 
+
+void pslist_task_init(struct task_struct *task) {
+    sprintf(name_buffer, "%d", task->pid);
+    kobject_create_and_add(name_buffer, root_kobj);
+} 
+
 static int __init start(void)
 {
-	int retval;
+    int retval;
+    struct task_struct* task;
+    
+    root_kobj = kobject_create_and_add("pslist", kernel_kobj);
+    if (!root_kobj)
+            return -ENOMEM;
+    
+//    retval = sysfs_create_group(root_kobj, &attr_group);
+//    if (retval)
+//            kobject_put(root_kobj);
+    
+    for_each_process(task) {
+        //printk(task-)
+        pslist_task_init(task);
+    }
 
-	root_kobj = kobject_create_and_add("pslist", kernel_kobj);
-	if (!root_kobj)
-		return -ENOMEM;
-
-	/* Create the files associated with this kobject */
-	retval = sysfs_create_group(root_kobj, &attr_group);
-	if (retval)
-		kobject_put(root_kobj);
-
-	return retval;
+    return retval;
 }
 
 static void __exit stop(void)
 {
-	kobject_put(root_kobj);
+    kobject_put(root_kobj);
 }
 
 module_init(start);
 module_exit(stop);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Greg Kroah-Hartman <greg@kroah.com>");
+MODULE_AUTHOR("Alexey Velikiy <gmpota@gmail.com>");
+
+
+
+
+
+
+
+
