0c9d6a3fe6e2aacc9e481edf29a4433345fe21a3
Alexey Velikiy <gmpota@gmail.com>
2012-07-04 11:17:32 +0400

    PR4 before

--- PR4.c
+++ PR4.c
@@ -9,48 +9,67 @@
 char name_buffer[40];
 static struct kobject *root_kobj;
 
-static ssize_t process_name_show(struct kobject *kobj, 
+static ssize_t name_show(struct kobject *kobj, 
                                 struct kobj_attribute *attr, 
                                 char *buf) {
-    //sprintf(buf, "Hello name");
-//       strncpy(buf,
-//               container_of(kobj, struct task_struct, comm), 
-//               TASK_COMM_LEN);
+    strncpy(buf,
+        container_of(kobj, struct task_struct, pslist_link)->comm, 
+        TASK_COMM_LEN);
+
     return 0;
 }
 
-static struct kobj_attribute process_name_attr  = __ATTR_RO(process_name);
+static struct kobj_attribute process_name_attr  = __ATTR_RO(name);
 
-void pslist_task_init(struct task_struct* task) {
-    struct kobject * new_task_ptr;
-    int res = 0;
-    sprintf(name_buffer, "%d", task->pid);
-    
-    new_task_ptr = kobject_create_and_add(name_buffer, root_kobj);
-    if(new_task_ptr == NULL) return;
-    res = sysfs_create_file(new_task_ptr, &process_name_attr);
+static struct attribute* process_attrs[] = {
+       &process_name_attr.attr,
+       NULL
+};
+
+static struct kobj_type process_kobj_type = {
+       .release = NULL,
+       .sysfs_ops = &kobj_sysfs_ops,
+       .default_attrs = process_attrs,
+       .child_ns_type = NULL,
+       .namespace = NULL
+};
+
+static struct kobj_type root_type = {
+       .release = NULL,
+       .sysfs_ops = &kobj_sysfs_ops,
+       .default_attrs = NULL,
+       .child_ns_type = NULL,
+       .namespace = NULL
+};
+
+static struct kobject kobj_root;
+
+void pslist_task_init(struct task_struct *child) {
+
+       memset(&child->pslist_link, 0, sizeof(struct kobject));
+
+       kobject_init(&child->pslist_link, &process_kobj_type);
+       kobject_add(&child->pslist_link, &kobj_root, "%d", child->pid);
 } 
 
-static int __init start(void)
-{
-    int retval = 0;
-    struct task_struct* task;
-    int q = 0;
-    
-    root_kobj = kobject_create_and_add("pslist", kernel_kobj);
-    if (!root_kobj) return -ENOMEM;
-    
-//    retval = sysfs_create_group(root_kobj, &attr_group);
-//    if (retval)
-//            kobject_put(root_kobj);
-    
-    
-//    for_each_process(task) {
-//        pslist_task_init(task);
-//        if(q++ > 10) break;
-//    }
+void pslist_task_link(struct task_struct *parent, struct task_struct *child) {
+       sysfs_create_link(&parent->pslist_link, 
+                         &child->pslist_link, 
+                         kobject_name(&child->pslist_link));
+}
 
-    return retval;
+void pslist_task_release(struct task_struct *tsk) {
+       kobject_del(&tsk->pslist_link);
+       kobject_put(&tsk->pslist_link);
+}
+
+static int __init pslist_init(void)
+{
+      kobject_init(&kobj_root, &root_type);
+      kobject_add(&kobj_root, kernel_kobj, "pslist");
+
+      //initialized = 1;
+
 }
 
 static void __exit stop(void)
@@ -59,7 +78,7 @@ static void __exit stop(void)
     kobject_put(root_kobj);
 }
 
-module_init(start);
+module_init(pslist_init);
 module_exit(stop);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alexey Velikiy <gmpota@gmail.com>");
