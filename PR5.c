be07e4822d2d918edfb8b830665262738b0cd824
Alexey Velikiy <gmpota@gmail.com>
2012-07-06 00:18:42 +0400

    Bu**sh*t

--- PR5.c
+++ PR5.c
@@ -10,15 +10,132 @@
 #include <linux/init.h>
 #include <linux/string.h>
 #include <linux/backing-dev.h>
-#include <linux/ramfs.h>
 #include <linux/sched.h>
 #include <linux/parser.h>
 #include <linux/magic.h>
 #include <linux/slab.h>
 
+#define MYFS_MAGIC 0x74736263
+
+/** Declarations */
+static struct file_system_type ramfs_fs_type;
+static const struct super_operations ramfs_ops;
+struct ramfs_mount_opts { umode_t mode; };
+struct ramfs_fs_info { struct ramfs_mount_opts mount_opts; };
+enum { Opt_mode, Opt_err };
+
+extern const struct inode_operations ramfs_file_inode_operations;
+extern const struct address_space_operations ramfs_aops;
+
+struct inode *ramfs_get_inode(struct super_block *sb,
+				const struct inode *dir, umode_t mode, dev_t dev)
+{
+	struct inode * inode = new_inode(sb);
+
+	if (inode) {
+		inode->i_ino = get_next_ino();
+		inode_init_owner(inode, dir, mode);
+		inode->i_mapping->a_ops = &ramfs_aops;
+		inode->i_mapping->backing_dev_info = &ramfs_backing_dev_info;
+		mapping_set_gfp_mask(inode->i_mapping, GFP_HIGHUSER);
+		mapping_set_unevictable(inode->i_mapping);
+		inode->i_atime = inode->i_mtime = inode->i_ctime = CURRENT_TIME;
+		switch (mode & S_IFMT) {
+		default:
+			init_special_inode(inode, mode, dev);
+			break;
+		case S_IFREG:
+			inode->i_op = &ramfs_file_inode_operations;
+			inode->i_fop = &ramfs_file_operations;
+			break;
+		case S_IFDIR:
+			inode->i_op = &ramfs_dir_inode_operations;
+			inode->i_fop = &simple_dir_operations;
+
+			/* directory inodes start off with i_nlink == 2 (for "." entry) */
+			inc_nlink(inode);
+			break;
+		case S_IFLNK:
+			inode->i_op = &page_symlink_inode_operations;
+			break;
+		}
+	}
+	return inode;
+}
+
+
+static int ramfs_parse_options(char *data, struct ramfs_mount_opts *opts)
+{
+	substring_t args[MAX_OPT_ARGS];
+	int option;
+	int token;
+	char *p;
+
+	opts->mode = RAMFS_DEFAULT_MODE;
+
+	while ((p = strsep(&data, ",")) != NULL) {
+		if (!*p)
+			continue;
+
+		token = match_token(p, tokens, args);
+		switch (token) {
+		case Opt_mode:
+			if (match_octal(&args[0], &option))
+				return -EINVAL;
+			opts->mode = option & S_IALLUGO;
+			break;
+		}
+	}
+
+	return 0;
+}
+
 int ramfs_fill_super(struct super_block *sb, void *data, int silent) 
 {
-    return 0;
+	struct ramfs_fs_info *fsi;
+	struct inode *inode = NULL;
+	struct dentry *root;
+	int err;
+
+	save_mount_options(sb, data);
+
+	fsi = kzalloc(sizeof(struct ramfs_fs_info), GFP_KERNEL);
+	sb->s_fs_info = fsi;
+	if (!fsi) {
+		err = -ENOMEM;
+		goto fail;
+	}
+
+	err = ramfs_parse_options(data, &fsi->mount_opts);
+	if (err)
+		goto fail;
+
+	sb->s_maxbytes		= MAX_LFS_FILESIZE;
+	sb->s_blocksize		= PAGE_CACHE_SIZE;
+	sb->s_blocksize_bits	= PAGE_CACHE_SHIFT;
+	sb->s_magic		= MYFS_MAGIC;
+	sb->s_op		= &ramfs_ops;
+	sb->s_time_gran		= 1;
+
+	inode = ramfs_get_inode(sb, NULL, S_IFDIR | fsi->mount_opts.mode, 0);
+	if (!inode) {
+		err = -ENOMEM;
+		goto fail;
+	}
+
+	root = d_alloc_root(inode);
+	sb->s_root = root;
+	if (!root) {
+		err = -ENOMEM;
+		goto fail;
+	}
+
+	return 0;
+fail:
+	kfree(fsi);
+	sb->s_fs_info = NULL;
+	iput(inode);
+	return err;
 }
 
 struct dentry *ramfs_mount(struct file_system_type *fs_type,
@@ -33,6 +150,11 @@ static void ramfs_kill_sb(struct super_block *sb)
 	kill_litter_super(sb);
 }
 
+static const struct super_operations ramfs_ops = {
+	.statfs		= simple_statfs,
+	.drop_inode	= generic_delete_inode,
+	.show_options	= generic_show_options,
+};
 
 static struct file_system_type ramfs_fs_type = {
 	.name		= "myfs",
@@ -45,18 +167,32 @@ static int __init start(void)
     return register_filesystem(&ramfs_fs_type);
 }
 
-static void __exit stop(void)
-{
-    printk("\n    --end--    \n");
-}
+
 
 module_init(start);
-module_exit(stop);
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Alexey Velikiy <gmpota@gmail.com>");
 
 
-
-
 /******************************************************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
